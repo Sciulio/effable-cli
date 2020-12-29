868a4d8cd898d1356f93cd2e4e622ea23f0df8ec
Sciulio <sciulio@msn.com>
2020-12-29 19:32:40 +0100

    added params separated generate file

---
+++ src/generate.js
@@ -0,0 +1,77 @@
+const { readdirSync, mkdirSync, existsSync, statSync, copyFile, copyFileSync } = require('fs');
+const { resolve, join, normalize, dirname, relative, basename, extname } = require('path');
+const inquirer = require('inquirer');
+const glob = require("glob")
+
+const executingPath = process.cwd();
+const templatesPath = normalize(join(__dirname, '../templates'));
+const templatesFolders = readdirSync(templatesPath);
+
+const generationQuestions = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: templatesFolders
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate(input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+];
+
+module.exports = () => {
+  console.log("Generate task!")
+
+  inquirer
+  .prompt(generationQuestions)
+  .then(answers => {
+    console.log(answers);
+
+    const {
+      'project-choice': projectChoice,
+      'project-name': projectName
+    } = answers;
+    const templatePath = join(templatesPath, projectChoice);
+    const outputPath = resolve(executingPath, projectName);
+
+    const ioItems = glob.sync(join(templatePath, '**/*'));
+
+    console.log(" - templatePath:", templatePath)
+    console.log(" - outputPath:", outputPath)
+    console.log(" - ioItems")
+    console.log(ioItems)
+
+    ioItems
+      .forEach(srcItem => {
+        const srcStats = statSync(srcItem);
+
+        const srcFolder = srcStats.isDirectory() ? srcItem : dirname(srcItem);
+        const outFolder = join(outputPath, relative(templatePath, srcFolder));
+        const outFile = srcStats.isFile() ? join(outFolder, basename(srcItem)) : null;
+
+        console.log(' - srcItem', srcItem)
+        outFile || console.log(' - - outFolder', outFolder)
+        outFile && console.log(' - - outFile', outFile);
+
+        if (!existsSync(outFolder)) {
+          mkdirSync(outFolder, {
+            recursive: true
+          });
+        }
+
+        if (outFile) {
+          const outFile = join(outFolder, basename(srcItem));
+
+          copyFileSync(srcItem, outFile);
+        }
+      });
+
+    createDirectoryContents(templatePath, projectName);
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,75 +1,13 @@
-const { readdirSync, mkdirSync, existsSync, statSync, copyFile, copyFileSync } = require('fs');
-const { resolve, join, normalize, dirname, relative, basename, extname } = require('path');
-const inquirer = require('inquirer');
-const glob = require("glob")
-
-const executingPath = process.cwd();
-const templatesPath = normalize(join(__dirname, '../templates'));
-const templatesFolders = readdirSync(templatesPath);
-
-const QUESTIONS = [
-  {
-    name: 'project-choice',
-    type: 'list',
-    message: 'What project template would you like to generate?',
-    choices: templatesFolders
-  },
-  {
-    name: 'project-name',
-    type: 'input',
-    message: 'Project name:',
-    validate(input) {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
-  }
-];
+const generate = require("./generate");
 
 
-inquirer
-.prompt(QUESTIONS)
-.then(answers => {
-  console.log(answers);
+const [task, ...args] = process.argv.slice(2);
 
-  const {
-    'project-choice': projectChoice,
-    'project-name': projectName
-  } = answers;
-  const templatePath = join(templatesPath, projectChoice);
-  const outputPath = resolve(executingPath, projectName);
 
-  console.log("templatePath")
-  console.log(templatePath)
-  console.log(outputPath)
-
-  const ioItems = glob.sync(join(templatePath, '**/*'));
-  console.log("ioItems")
-  console.log(ioItems)
-
-  ioItems
-  .forEach(srcItem => {
-    const srcStats = statSync(srcItem);
-    
-    const srcFolder = srcStats.isDirectory() ? srcItem : dirname(srcItem);
-    const outFolder = join(outputPath, relative(templatePath, srcFolder));
-    const outFile = srcStats.isFile() ? join(outFolder, basename(srcItem)) : null;
-
-    console.log('srcItem', srcItem)
-    outFile || console.log(' - outFolder', outFolder)
-    outFile && console.log(' - outFile', outFile);
-
-    if (!existsSync(outFolder)) {
-      mkdirSync(outFolder, {
-        recursive: true
-      });
-    }
-
-    if (outFile) {
-      const outFile = join(outFolder, basename(srcItem));
-
-      copyFileSync(srcItem, outFile);
-    }
-  });
-
-  createDirectoryContents(templatePath, projectName);
-});
+switch(task) {
+  case 'generate':
+    generate();
+    break;
+  default:
+    console.log('Nothing to do!');
+}
